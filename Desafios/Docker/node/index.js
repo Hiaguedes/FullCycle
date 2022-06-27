4dfe3b824640adaffee838f9fbcc9bcae52972bd
Hiaguedes <hiagorguedes@gmail.com>
2022-06-27 14:46:53 -0300

    chore: usando o nginx pra fazer conexao com proxy reverso

--- Desafios/Docker/node/index.js
+++ Desafios/Docker/node/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express();
 const PORT = 4000;
+const HOST = '0.0.0.0';
 
 app.get('/', (req, res) => {
     res.send('<h1>Full Cycle Rocks!</h1>')
   })
   
   app.listen(PORT, () => {
-    console.log(`Ouvindo na porta ${PORT}`)
+    console.log(`Running on http://${HOST}:${PORT}`)
   })
