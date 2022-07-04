a88b8d47a709d5fc6a5df7dbcc4832c1d8294fc0
Hiaguedes <hiagorguedes@gmail.com>
2022-07-04 20:20:42 -0300

    feat(desafio docker node nginx mysql): :construction: comecando os trabalhos com mysql e docker

--- Desafios/Docker/node/index.js
+++ Desafios/Docker/node/index.js
@@ -3,7 +3,32 @@ const app = express();
 const PORT = 4000;
 const HOST = '0.0.0.0';
 
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Hiago')`
+connection.query(sql)
+connection.end()
+
 app.get('/', (req, res) => {
+    var queryString = 'SELECT name FROM people';
+
+    connection.query(queryString, function(err, rows, fields) {
+        if (err) throw err;
+    
+        for (var i in rows) {
+            console.log('Post Titles: ', rows[i]);
+        }
+    });
+
+    connection.end()
+
     res.send('<h1>Full Cycle Rocks!</h1>')
   })
   
